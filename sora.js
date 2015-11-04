b898dccb9a0593e4dba09db708d5641dba42158b
Yuki Ito <yuitowest@gmail.com>
2015-11-04 16:00:33 +0900

    全体的に設計しなおし

--- sora.js
+++ sora.js
@@ -1,28 +1,50 @@
 (function(global) {
   "use strict";
 
-  var Sora = function() {};
-  Sora.prototype.connect = function(params, onSuccess, onError) {
-    var ws = new WebSocket("ws://" + params.host + ":" + params.port + "/" + params.path);
+  var Sora = function(config) {
+    this.config = config || {};
+  };
+
+  Sora.prototype.connection = function(onSuccess, onError) {
+    var ws = new WebSocket("ws://" + this.config.host + ":" + this.config.port + "/" + this.config.path);
     ws.onopen = function() {
-      var message = JSON.stringify({
-        "type": params.role,
-        "channelId": params.channelId
-      });
-      ws.send(message);
+      onSuccess();
     };
-    ws.onmessage = function(event) {
+    ws.onerror = function(e) {
+      onError(e);
+    };
+    return new SoraConnection(ws);
+  };
+
+  var SoraConnection = function(ws) {
+    this._ws = ws;
+    this._onOffer = function(message) {};
+  };
+
+  SoraConnection.prototype.signaling = function(params) {
+    var message = JSON.stringify({
+      "type": "connect",
+      "role": params.role,
+      "channelId": params.channelId
+    });
+    var self = this;
+    this._ws.send(message);
+    this._ws.onmessage = function(event) {
       var message = JSON.parse(event.data);
       if (message.type == "offer") {
-        onSuccess(message);
+        self._onOffer(message);
       } else if (message.type == "ping") {
-        ws.send(JSON.stringify({type: "pong"}));
+        self._ws.send(JSON.stringify({type: "pong"}));
       }
     };
-    ws.onclose = function(error) {
-      onError(error);
-    };
-    return ws;
+  };
+
+  SoraConnection.prototype.offer = function(callback) {
+    this._onOffer = callback;
+  };
+
+  SoraConnection.prototype.candidate = function(sdp) {
+    this._ws.send(JSON.stringify({"type": "answer", "sdp": sdp}));
   };
 
   if ("process" in global) {
