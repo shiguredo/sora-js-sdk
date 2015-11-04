e7aad780b07f0fba983add641366142ba740bb24
Yuki Ito <yuitowest@gmail.com>
2015-11-04 16:15:27 +0900

    順番入替えする

--- example/upstream/up.js
+++ example/upstream/up.js
@@ -8,35 +8,31 @@ var config = {
   "iceServers": [{"urls": "stun:stun.l.google.com:19302"}]
 };
 var pc = new RTCPeerConnection(config);
-var localStream;
 
 var sora = new Sora({"host": "127.0.0.1", "port": 5000, "path": "signaling"});
-var connection = null;
-
-navigator.getUserMedia({video: true, audio: true}, function(stream) {
-  localStream = stream;
-  var localVideo = document.getElementById("local-video");
-  localVideo.src = window.URL.createObjectURL(stream);
-  localVideo.play();
-  connection = sora.connection(onSuccess, onError);
-}, onError);
+var connection = sora.connection(onSuccess, onError);
 
 function onSuccess() {
   connection.signaling({"role": "upstream", "channelId": "sora"});
-  connection.offer(function(message) {
-    pc.addStream(localStream);
-    pc.setRemoteDescription(new RTCSessionDescription(message), function() {
-      pc.createAnswer(function(answer) {
-        pc.setLocalDescription(answer, function() {
-          pc.onicecandidate = function(event) {
-            if (event.candidate === null) {
-              connection.candidate(pc.localDescription.sdp);
+  navigator.getUserMedia({video: true, audio: true}, function(stream) {
+    var localVideo = document.getElementById("local-video");
+    localVideo.src = window.URL.createObjectURL(stream);
+    localVideo.play();
+    connection.offer(function(message) {
+      pc.addStream(stream);
+      pc.setRemoteDescription(new RTCSessionDescription(message), function() {
+        pc.createAnswer(function(answer) {
+          pc.setLocalDescription(answer, function() {
+            pc.onicecandidate = function(event) {
+              if (event.candidate === null) {
+                connection.candidate(pc.localDescription.sdp);
+              }
             }
-          }
+          }, onError);
         }, onError);
       }, onError);
-    }, onError);
-  });
+    });
+  }, onError);
 }
 
 function onError(error) {
