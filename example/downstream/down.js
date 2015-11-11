a0debdfed101d6ee02b428cbc66558f35300a794
Yuki Ito <yuitowest@gmail.com>
2015-11-11 18:10:18 +0900

    downstream の example を修正する

--- example/downstream/down.js
+++ example/downstream/down.js
@@ -5,30 +5,34 @@ var config = {
 };
 var pc = new RTCPeerConnection(config);
 
-var sora = new Sora({"host": "127.0.0.1", "port": 5000, "path": "signaling"});
-var connection = sora.connection(onSuccess, onError);
-
-function onSuccess() {
-  connection.connect({"role": "downstream", "channelId": "sora"}, function(message) {
-    pc.setRemoteDescription(new RTCSessionDescription(message), function() {
-      pc.createAnswer(function(answer) {
-        pc.setLocalDescription(answer, function() {
-          pc.onicecandidate = function(event) {
-            if (event.candidate === null) {
-              connection.answer(pc.localDescription.sdp);
-            }
-          };
+$("#start").on("click", function () {
+  var sora = new Sora({"host": "127.0.0.1", "port": 5000, "path": "signaling"});
+  var connection = sora.connection(onSuccess, onError);
+  function onSuccess() {
+    connection.connect(
+      {"role": "downstream", "channelId": $("#channel").val(), "accessToken": $("#token").val()},
+      function(message) {
+        pc.setRemoteDescription(new RTCSessionDescription(message), function() {
+          pc.createAnswer(function(answer) {
+            pc.setLocalDescription(answer, function() {
+              pc.onicecandidate = function(event) {
+                if (event.candidate === null) {
+                  connection.answer(pc.localDescription.sdp);
+                }
+              };
+            }, onError);
+          }, onError);
         }, onError);
-      }, onError);
-    }, onError);
-    pc.onaddstream = function(event) {
-      var remoteVideo = document.getElementById("remote-video");
-      remoteVideo.src = window.URL.createObjectURL(event.stream);
-      remoteVideo.play();
-    };
-  }, onError);
-}
+        pc.onaddstream = function(event) {
+          var remoteVideo = document.getElementById("remote-video");
+          remoteVideo.src = window.URL.createObjectURL(event.stream);
+          remoteVideo.play();
+        };
+      }, onError
+    );
+  }
 
-function onError(error) {
-  console.warn(error);
-}
+  function onError(error) {
+    console.warn(error);
+  }
+});
