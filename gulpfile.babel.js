931a30f65bb8372a7fb807c28719cb70e057074f
Yuki Ito <yuitowest@gmail.com>
2015-11-18 16:33:54 +0900

    gulp に minify task を追加する

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,7 @@
 import browserify from "browserify";
 import gulp from "gulp";
+import uglify from "gulp-uglify";
+import rename from "gulp-rename";
 import runSequence from "run-sequence";
 import buffer from "vinyl-buffer";
 import source from "vinyl-source-stream";
@@ -8,10 +10,10 @@ import source from "vinyl-source-stream";
 gulp.task("dist", callback => {
   return runSequence.use(gulp)(
       "compile",
+      "uglify",
       callback
       );
 });
-
 
 gulp.task("compile", () => {
   return browserify({
@@ -24,4 +26,11 @@ gulp.task("compile", () => {
   .pipe(source("sora.js"))
   .pipe(buffer())
   .pipe(gulp.dest("dist"));
+});
+
+gulp.task("uglify", () => {
+  return gulp.src(["dist/sora.js"])
+    .pipe(uglify())
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest("dist"));
 });
