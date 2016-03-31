f580338c27d752d3b735a80dfd9c6bbe1e37e2c7
Yuki Ito <yuitowest@gmail.com>
2016-03-31 14:44:22 +0900

    example を修正する

--- example/upstream/up.js
+++ example/upstream/up.js
@@ -11,45 +11,38 @@ var pc = new RTCPeerConnection(config);
 
 $("#start").on("click", function () {
   var sora = new Sora("ws://127.0.0.1:5000/signaling");
-  var connection = sora.connection(onSuccess, onError, onClose);
+  var connection = sora.connection();
 
-  function onSuccess() {
-    navigator.getUserMedia({video: true, audio: true}, function(stream) {
-      var localVideo = document.getElementById("local-video");
-      localVideo.src = window.URL.createObjectURL(stream);
-      localVideo.play();
-      connection.connect(
-        {"role": "upstream", "channelId": $("#channel").val(), "accessToken": $("#token").val()},
-        function(message) {
-          console.log("--- offer sdp ---");
-          console.log(message.sdp);
-          pc.addStream(stream);
-          pc.setRemoteDescription(new RTCSessionDescription(message), function() {
-            pc.createAnswer(function(answer) {
-              pc.setLocalDescription(answer, function() {
-                console.log("--- answer sdp ---");
-                console.log(answer.sdp);
-                connection.answer(answer.sdp);
-                pc.onicecandidate = function(event) {
-                  if (event.candidate !== null) {
-                    console.log("--- candidate ---");
-                    console.log(event.candidate);
-                    connection.candidate(event.candidate);
-                  }
-                };
-              }, onError);
+  navigator.getUserMedia({video: true, audio: true}, function(stream) {
+    var localVideo = document.getElementById("local-video");
+    localVideo.src = window.URL.createObjectURL(stream);
+    localVideo.play();
+    var params = {
+      role: "upstream",
+      channelId: $("#channel").val(),
+      accessToken: $("#token").val(),
+      codecType: $("select[name=codec-type]").val()
+    };
+    connection
+      .connect(params)
+      .then(function(offer) {
+        pc.addStream(stream);
+        pc.setRemoteDescription(new RTCSessionDescription(offer), function() {
+          pc.createAnswer(function(answer) {
+            pc.setLocalDescription(answer, function() {
+              connection.answer(answer.sdp);
+              pc.onicecandidate = function(event) {
+                if (event.candidate !== null) {
+                  connection.candidate(event.candidate);
+                }
+              };
             }, onError);
           }, onError);
-        }, onError
-      );
-    }, onError);
-  }
+        }, onError);
+      });
+  }, onError);
 
   function onError(error) {
-    console.warn(error);
-  }
-
-  function onClose(error) {
     console.warn(error);
   }
 });
