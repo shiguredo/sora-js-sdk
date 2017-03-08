bd8fb6e7500a952a385e19f74addf66fd9ffaed8
Yuki Ito <yuito@shiguredo.jp>
2017-03-08 18:36:40 +0900

    gulp を捨てて webpack2 にする

--- karma.conf.js
+++ karma.conf.js
@@ -1,28 +1,52 @@
 module.exports = function(config) {
   config.set({
     basePath: '',
-    frameworks: ['mocha', 'browserify'],
+    frameworks: ['mocha'],
     files: [
       'test/**/*.spec.js'
     ],
     exclude: [
     ],
     preprocessors: {
-      'test/**/*.spec.js': ['browserify']
-    },
-    browserify: {
-      debug: true,
-      transform: [
-        ['babelify', { presets: ['es2015'] }]
-      ]
+      'test/**/*.spec.js': ['webpack']
     },
     reporters: ['progress'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['Chrome'],
+    logLevel: config.LOG_WARN,
+    autoWatch: true,
+    customLaunchers: {
+      chrome_without_security: {
+        base: 'Chrome',
+        flags: [
+          '--disable-user-media-security',
+          '--use-fake-ui-for-media-stream',
+          '--use-fake-device-for-media-stream'
+        ],
+        displayName: 'Chrome w/o security'
+      }
+    },
+    browsers: ['chrome_without_security'],
     singleRun: false,
-    concurrency: Infinity
+    concurrency: Infinity,
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
+    webpack: {
+      devtool: 'inline-source-map',
+      module: {
+        exprContextCritical: false,
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: [/node_modules/],
+            use: [{
+              loader: 'babel-loader',
+              options: { presets: ['es2015'],  plugins: ['transform-flow-strip-types'] }
+            }],
+          },
+        ],
+      }
+    }
   });
 };
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+/* global __dirname */
+
+const path = require('path');
+const webpack = require('webpack');
+const pkg = require('./package.json');
+const env = process.env.NODE_ENV || 'development';
+const banner = pkg.name + '\n' + pkg.description +
+  '\n@version: ' + pkg.version + '\n@author: ' + pkg.author + '\n@license: ' + pkg.license;
+
+
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    sora: './sora.js',
+    "sora.min": './sora.js',
+  },
+  output: {
+    library: "Sora",
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+  },
+  plugins: [
+    new webpack.BannerPlugin(banner),
+    new webpack.optimize.UglifyJsPlugin({ include: /\.min\.js$/, minimize: true })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015'],  plugins: ['transform-flow-strip-types'] }
+        }],
+      },
+    ],
+  },
+};
