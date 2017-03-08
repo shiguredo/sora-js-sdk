1eae70d4fe2a2dec5539adba4cde4e9df350b06b
Yuki Ito <yuito@shiguredo.jp>
2017-03-08 18:38:15 +0900

    テストを修正する

--- test/sora.spec.js
+++ test/sora.spec.js
@@ -5,176 +5,35 @@ import Sora from '../src/sora';
 
 
 const channelId = '7N3fsMHob';
-const accessToken = 'PG9A6RXgYqiqWKOVO';
+const metadata = 'PG9A6RXgYqiqWKOVO';
 const signalingUrl = 'ws://127.0.0.1:5000/signaling';
 
 describe('Sora', () => {
   describe('Connection', () => {
     it('Success publisher connect', (done) => {
       const sora = Sora.connection(signalingUrl);
-      const connection = sora.connection();
-      const params = {
-        role: 'upstream',
-        channelId: channelId,
-        accessToken: accessToken
-      };
-      connection.connect(params)
-        .then(() => {
-          done();
+      const publisher = sora.publisher(channelId, metadata);
+      navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+        .then(mediaStream => {
+          publisher.connect(mediaStream)
+            .then(_ => {
+              done();
+            });
         })
-        .catch(() => {
-          assert(false, 'Faild signaling.');
+        .catch(e => {
+          assert(false, 'Faild publisher signaling.' + e);
         });
     });
-  });
-
-  describe('Params', () => {
-    let connection = new Sora(signalingUrl).connection();
-
-    it('simple', () => {
-      const params = {
-        role: 'upstream',
-        channelId: channelId
-      };
-      const actual = connection._createSignalingMessage(params);
-      const expected = {
-        type: 'connect',
-        role: 'upstream',
-        channel_id: channelId,
-        access_token: null,
-        audio: true,
-        video: true
-      };
-      assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
-    });
-
-    it('undefined to null', () => {
-      const actual = connection._createSignalingMessage({});
-      const expected = {
-        type: 'connect',
-        role: null,
-        channel_id: null,
-        access_token: null,
-        audio: true,
-        video: true
-      };
-      assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
-    });
-
-    it('audio and video', () => {
-      const params = {
-        audio: false,
-        video: false
-      };
-      const actual = connection._createSignalingMessage(params);
-      const expected = {
-        type: 'connect',
-        role: null,
-        channel_id: null,
-        access_token: null,
-        audio: false,
-        video: false
-      };
-      assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
-    });
-
-    describe('audioCodecType parameter', () => {
-      it('audio true', () => {
-        const params = {
-          audio: true,
-          audioCodecType: 'OPUS'
-        };
-        const actual = connection._createSignalingMessage(params);
-        const expected = {
-          type: 'connect',
-          role: null,
-          channel_id: null,
-          access_token: null,
-          audio: {
-            codec_type: 'OPUS'
-          },
-          video: true
-        };
-        assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
-      });
-
-      it('audio false', () => {
-        const params = {
-          audio: false,
-          audioCodecType: 'OPUS'
-        };
-        const actual = connection._createSignalingMessage(params);
-        const expected = {
-          type: 'connect',
-          role: null,
-          channel_id: null,
-          access_token: null,
-          audio: false,
-          video: true
-        };
-        assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
-      });
-    });
-
-    describe('video parameter', () => {
-      it('video true', () => {
-        const params = {
-          video: true,
-          videoCodecType: 'VP8'
-        };
-        const actual = connection._createSignalingMessage(params);
-        const expected = {
-          type: 'connect',
-          role: null,
-          channel_id: null,
-          access_token: null,
-          audio: true,
-          video: {
-            codec_type: 'VP8'
-          }
-        };
-        assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
-      });
-
-      it('video false', () => {
-        const params = {
-          video: false,
-          videoCodecType: 'VP8'
-        };
-        const actual = connection._createSignalingMessage(params);
-        const expected = {
-          type: 'connect',
-          role: null,
-          channel_id: null,
-          access_token: null,
-          audio: true,
-          video: false
-        };
-        assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
-      });
-
-      it('videoCodecType videoBitRate videoSnapshot', () => {
-        const params = {
-          video: true,
-          videoCodecType: 'VP8',
-          videoBitRate: 50,
-          videoSnapshot: true
-        };
-        const actual = connection._createSignalingMessage(params);
-        const expected = {
-          type: 'connect',
-          role: null,
-          channel_id: null,
-          access_token: null,
-          audio: true,
-          video: {
-            codec_type: 'VP8',
-            bit_rate: 50,
-            snapshot: true
-          }
-        };
-        assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
-      });
+    it('Success subscriber connect', (done) => {
+      const sora = Sora.connection(signalingUrl);
+      const subscriber = sora.subscriber(channelId, metadata);
+      subscriber.connect()
+        .then(_ => {
+          done();
+        })
+        .catch(e => {
+          assert(false, 'Faild subscriber signaling.' + e);
+        });
     });
   });
 });
---
+++ test/utils.spec.js
@@ -0,0 +1,174 @@
+/* global describe:false, it:false */
+
+import assert from 'power-assert';
+import { createSignalingMessage } from '../src/utils';
+
+const role = 'upstream';
+const channelId = '7N3fsMHob';
+const metadata = 'PG9A6RXgYqiqWKOVO';
+
+describe('Utils', () => {
+  describe('createSignalingMessage', () => {
+    it('simple', () => {
+      const actual = createSignalingMessage(role, channelId, metadata, {});
+      const expected = {
+        type: 'connect',
+        role: role,
+        channel_id: channelId,
+        access_token: metadata,
+        audio: true,
+        video: true
+      };
+      assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
+    });
+
+    it('undefined to null', () => {
+      const actual = createSignalingMessage(undefined, undefined, undefined, {});
+      const expected = {
+        type: 'connect',
+        role: null,
+        channel_id: null,
+        access_token: null,
+        audio: true,
+        video: true
+      };
+      assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
+    });
+
+    it('audio and video', () => {
+      const options = {
+        audio: false,
+        video: false
+      };
+      const actual = createSignalingMessage(role, channelId, metadata, options);
+      const expected = {
+        type: 'connect',
+        role: role,
+        channel_id: channelId,
+        access_token: metadata,
+        audio: false,
+        video: false
+      };
+      assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
+    });
+
+    describe('audioCodecType parameter', () => {
+      it('audio true', () => {
+        const options = {
+          audio: true,
+          audioCodecType: 'OPUS'
+        };
+        const actual = createSignalingMessage(role, channelId, metadata, options);
+        const expected = {
+          type: 'connect',
+          role: role,
+          channel_id: channelId,
+          access_token: metadata,
+          audio: {
+            codec_type: 'OPUS'
+          },
+          video: true
+        };
+        assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
+      });
+
+      it('audio false', () => {
+        const options = {
+          audio: false,
+          audioCodecType: 'OPUS'
+        };
+        const actual = createSignalingMessage(role, channelId, metadata, options);
+        const expected = {
+          type: 'connect',
+          role: role,
+          channel_id: channelId,
+          access_token: metadata,
+          audio: false,
+          video: true
+        };
+        assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
+      });
+    });
+
+    describe('video parameter', () => {
+      it('video true', () => {
+        const options = {
+          video: true,
+          videoCodecType: 'VP8'
+        };
+        const actual = createSignalingMessage(role, channelId, metadata, options);
+        const expected = {
+          type: 'connect',
+          role: role,
+          channel_id: channelId,
+          access_token: metadata,
+          audio: true,
+          video: {
+            codec_type: 'VP8'
+          }
+        };
+        assert.deepEqual(actual, expected);
+      });
+
+      it('video false', () => {
+        const options = {
+          video: false,
+          videoCodecType: 'VP8'
+        };
+        const actual = createSignalingMessage(role, channelId, metadata, options);
+        const expected = {
+          type: 'connect',
+          role: role,
+          channel_id: channelId,
+          access_token: metadata,
+          audio: true,
+          video: false
+        };
+        assert.deepEqual(actual, expected);
+      });
+
+      it('videoCodecType videoBitRate videoSnapshot', () => {
+        const options = {
+          video: true,
+          videoCodecType: 'VP8',
+          videoBitRate: 50,
+          videoSnapshot: true
+        };
+        const actual = createSignalingMessage(role, channelId, metadata, options);
+        const expected = {
+          type: 'connect',
+          role: role,
+          channel_id: channelId,
+          access_token: metadata,
+          audio: true,
+          video: {
+            codec_type: 'VP8',
+            bit_rate: 50,
+            snapshot: true
+          }
+        };
+        assert.deepEqual(actual, expected);
+      });
+    });
+
+    describe('multistream parameter', () => {
+      it('multistream', () => {
+        const options = {
+          multistream: true
+        };
+        const actual = createSignalingMessage(role, channelId, metadata, options);
+        const expected = {
+          type: 'connect',
+          role: role,
+          channel_id: channelId,
+          access_token: metadata,
+          multistream: true,
+          plan_b: true,
+          video: true,
+          audio: true,
+        };
+        assert.deepEqual(actual, expected);
+      });
+    });
+  });
+});
