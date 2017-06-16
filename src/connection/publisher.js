942827689050f64aa2431a6461dd52383fcba9fa
Yuki Ito <yuito@shiguredo.jp>
2017-06-16 15:54:32 +0900

    不要コードを削除する

--- src/connection/publisher.js
+++ src/connection/publisher.js
@@ -60,14 +60,12 @@ class ConnectionPublisher extends ConnectionBase {
             }
           };
         } else {
-          this.streams = [];
           this._pc.ontrack = event => {
             const stream = event.streams[0];
             if (stream.id === 'default') return;
             if (stream.id === this.clientId) return;
             if (event.track.kind === 'video') {
               event.stream = stream;
-              this.streams.push(stream);
               this._callbacks.addstream(event);
             }
           };
