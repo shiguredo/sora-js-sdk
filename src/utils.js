c0d302ed4da9e71fac622db87d493db35ed16871
Yuki Ito <yuito@shiguredo.jp>
2018-10-01 12:06:27 +0900

    UserAgent 判定を修正する

--- src/utils.js
+++ src/utils.js
@@ -17,17 +17,20 @@ export function trace(clientId: ?string, title: string, value: Object | string) 
 
 function browser() {
   const ua = window.navigator.userAgent.toLocaleLowerCase();
-  if (ua.indexOf('chrome') !== -1) {
+  if (ua.indexOf('edge') !== -1) {
+    return 'edge';
+  }
+  else if (ua.indexOf('chrome')  !== -1 && ua.indexOf('edge') === -1) {
     return 'chrome';
   }
-  else if (ua.indexOf('edge') !== -1) {
-    return 'edge';
+  else if (ua.indexOf('safari')  !== -1 && ua.indexOf('chrome') === -1) {
+    return 'safari';
+  }
+  else if (ua.indexOf('opera')   !== -1) {
+    return 'opera';
   }
   else if (ua.indexOf('firefox') !== -1) {
     return 'firefox';
-  }
-  else if (ua.indexOf('safari') !== -1) {
-    return 'safari';
   }
   return ;
 }
