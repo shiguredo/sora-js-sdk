15ebfce4b897884d198bcf5da413c5ea8c09922e
Yuki Ito <yuito@shiguredo.jp>
2019-07-31 12:27:00 +0900

    テストを修正する

--- test/sora.spec.js
+++ test/sora.spec.js
@@ -1,8 +1,6 @@
-/* global describe:false, it:false */
+/* global describe:false, it:false, assert:false */
 
-import assert from 'power-assert';
 import Sora from '../src/sora';
-
 
 const channelId = '7N3fsMHob';
 const metadata = 'PG9A6RXgYqiqWKOVO';
@@ -10,24 +8,25 @@ const signalingUrl = 'ws://127.0.0.1:5000/signaling';
 
 describe('Sora', () => {
   describe('Connection', () => {
-    it('Success publisher connect', (done) => {
+    it('Success publisher connect', done => {
       const sora = Sora.connection(signalingUrl);
       const publisher = sora.publisher(channelId, metadata);
-      navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-        .then(mediaStream => {
-          publisher.connect(mediaStream)
-            .then(_ => {
-              done();
-            });
-        })
-        .catch(e => {
-          assert(false, 'Faild publisher signaling.' + e);
-        });
+      navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(mediaStream => {
+        publisher
+          .connect(mediaStream)
+          .then(_ => {
+            done();
+          })
+          .catch(e => {
+            assert(false, 'Faild publisher signaling.' + e);
+          });
+      });
     });
-    it('Success subscriber connect', (done) => {
+    it('Success subscriber connect', done => {
       const sora = Sora.connection(signalingUrl);
       const subscriber = sora.subscriber(channelId, metadata);
-      subscriber.connect()
+      subscriber
+        .connect()
         .then(_ => {
           done();
         })
