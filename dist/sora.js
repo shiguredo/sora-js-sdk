98d993094c19cc093f42e29a1aece859b7fe464d
NAKAI Ryosuke <nakai@shiguredo.jp>
2020-06-01 14:51:05 +0900

    yarn release

--- dist/sora.js
+++ dist/sora.js
@@ -1,7 +1,7 @@
 /**
  * sora-js-sdk
  * WebRTC SFU Sora JavaScript SDK
- * @version: 2020.1.0-dev
+ * @version: 2020.1.0
  * @author: Shiguredo Inc.
  * @license: Apache-2.0
  **/
@@ -97,7 +97,7 @@
           type: "connect",
           // @ts-ignore
           // eslint-disable-next-line @typescript-eslint/camelcase
-          sora_client: `Sora JavaScript SDK ${'2020.1.0-dev'}`,
+          sora_client: `Sora JavaScript SDK ${'2020.1.0'}`,
           environment: window.navigator.userAgent,
           role: role,
           // eslint-disable-next-line @typescript-eslint/camelcase
@@ -885,7 +885,7 @@
       },
       version: function () {
           // @ts-ignore
-          return '2020.1.0-dev';
+          return '2020.1.0';
       },
   };
 
