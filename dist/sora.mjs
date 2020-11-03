298b9d84cf2c38547ffcc8066777e77bd0f3b285
NAKAI Ryosuke <nakai@shiguredo.jp>
2020-11-03 15:18:34 +0900

    成果物を追加する

--- dist/sora.mjs
+++ dist/sora.mjs
@@ -1,7 +1,7 @@
 /**
  * sora-js-sdk
  * WebRTC SFU Sora JavaScript SDK
- * @version: 2020.4.0
+ * @version: 2020.4.0-dev
  * @author: Shiguredo Inc.
  * @license: Apache-2.0
  **/
@@ -95,7 +95,7 @@ function createSignalingMessage(offerSDP, role, channelId, metadata, options) {
         type: "connect",
         // @ts-ignore
         // eslint-disable-next-line @typescript-eslint/camelcase
-        sora_client: `Sora JavaScript SDK ${'2020.4.0'}`,
+        sora_client: `Sora JavaScript SDK ${'2020.4.0-dev'}`,
         environment: window.navigator.userAgent,
         role: role,
         // eslint-disable-next-line @typescript-eslint/camelcase
@@ -123,16 +123,14 @@ function createSignalingMessage(offerSDP, role, channelId, metadata, options) {
             }
         }
     }
-    if ("simulcast" in options || "simulcastQuality" in options) {
+    if ("simulcast" in options || "simulcastRid" in options) {
         // simulcast
         if ("simulcast" in options && options.simulcast === true) {
             message.simulcast = true;
         }
-        const simalcastQualities = ["low", "middle", "high"];
-        if (options.simulcastQuality !== undefined && 0 <= simalcastQualities.indexOf(options.simulcastQuality)) {
-            message.simulcast = {
-                quality: options.simulcastQuality,
-            };
+        const simalcastRids = ["r0", "r1", "r2"];
+        if (options.simulcastRid !== undefined && 0 <= simalcastRids.indexOf(options.simulcastRid)) {
+            message.simulcast_rid = options.simulcastRid;
         }
     }
     // client_id
@@ -877,7 +875,7 @@ var sora = {
     },
     version: function () {
         // @ts-ignore
-        return '2020.4.0';
+        return '2020.4.0-dev';
     },
 };
 
