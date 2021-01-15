4d2065f59c069ee2c17abd271c5e1cfc629dc4d5
Yuki Ito <yuito@shiguredo.jp>
2021-01-15 16:13:02 +0900

    MediaStreamTrack applyConstraints の wrapper を追加する

--- packages/sdk/src/helpers.ts
+++ packages/sdk/src/helpers.ts
@@ -72,4 +72,26 @@ async function startAudioMediaDevice(
   mediastream.addTrack(newAudioTrack);
 }
 
-export { stopVideoMediaDevice, startVideoMediaDevice, stopAudioMediaDevice, startAudioMediaDevice };
+async function applyMediaStreamConstraints(
+  mediastream: MediaStream,
+  constraints: MediaStreamConstraints
+): Promise<void> {
+  if (constraints.audio && typeof constraints.audio !== "boolean") {
+    for (const track of mediastream.getAudioTracks()) {
+      await track.applyConstraints(constraints.audio);
+    }
+  }
+  if (constraints.video && typeof constraints.video !== "boolean") {
+    for (const track of mediastream.getVideoTracks()) {
+      await track.applyConstraints(constraints.video);
+    }
+  }
+}
+
+export {
+  applyMediaStreamConstraints,
+  stopVideoMediaDevice,
+  startVideoMediaDevice,
+  stopAudioMediaDevice,
+  startAudioMediaDevice,
+};
