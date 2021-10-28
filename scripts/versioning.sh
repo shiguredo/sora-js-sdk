d00063cadd9b019aa17d7dd0984aac95500ed8ae
Yuki Ito <yuito@shiguredo.jp>
2021-10-28 15:38:48 +0900

    version 更新コマンドを修正する

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -4,17 +4,29 @@ version=`cat ./package.json | jq -r -c ".version"`
 
 if [ $1 = "canary" ]; then
   if [[ ${version} =~ ^.*canary.*$ ]]; then
-    npx lerna version prerelease --preid canary --no-git-tag-version --tag-version-prefix '';
+    npm version prerelease --preid canary --no-git-tag-version;
   else
-    npx lerna version preminor --preid canary --no-git-tag-version --tag-version-prefix '';
+    npm version preminor --preid canary --no-git-tag-version;
   fi
 elif [ $1 = "major" ]; then
-  npx lerna version major --no-git-tag-version --tag-version-prefix '';
+  npm version major --no-git-tag-version --tag-version-prefix '';
 elif [ $1 = "minor" ]; then
-  npx lerna version minor --no-git-tag-version --tag-version-prefix '';
+  npm version minor --no-git-tag-version --tag-version-prefix '';
 elif [ $1 = "patch" ]; then
-  npx lerna version patch --no-git-tag-version --tag-version-prefix '';
+  npm version patch --no-git-tag-version --tag-version-prefix '';
 fi
 
-next_version=`cat ./lerna.json | jq -r -c ".version"`
-npm version ${next_version} --git-tag-version false;
+next_version=`cat ./package.json | jq -r -c ".version"`
+
+echo "Next version is '${next_version}'"
+read -p "Do you wish to git commit and tag this version? (y/n)" answer
+echo $answer
+case $answer in
+  "" | "Y" | "y" | "yes" | "Yes" | "YES" )
+    npm run build
+    git add .
+    git commit -m "${next_version}"
+    git tag ${next_version}
+    ;;
+  * ) exit 0;;
+esac
