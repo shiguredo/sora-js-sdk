e9ab876c5c2b04191a070807fc6e30775c646e25
Yuki Ito <yuito@shiguredo.jp>
2022-07-19 10:53:35 +0900

    rollup-plugin-terser を削除する

--- packages/sdk/rollup.config.js
+++ packages/sdk/rollup.config.js
@@ -1,6 +1,5 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import replace from '@rollup/plugin-replace';
 import pkg from '../../package.json';
@@ -31,32 +30,6 @@ export default [
     output: {
       sourcemap: false,
       file: '../../dist/sora.js',
-      format: 'umd',
-      name: 'Sora',
-      banner: banner
-    }
-  },
-  {
-    input: 'src/sora.ts',
-    plugins: [
-      replace({
-        __SORA_JS_SDK_VERSION__: pkg.version,
-        preventAssignment: true
-      }),
-      resolve(),
-      typescript({
-        tsconfig: './tsconfig.json'
-      }),
-      commonjs(),
-      terser({
-        output: {
-          comments: false,
-        },
-      })
-    ],
-    output: {
-      sourcemap: true,
-      file: '../../dist/sora.min.js',
       format: 'umd',
       name: 'Sora',
       banner: banner
