4374b7b7e30a25150a9f44eac6f4d2678b18da92
Takeru Ohta <phjgt308@gmail.com>
2022-12-13 15:35:36 +0900

    ビルド物を更新

--- dist/sora.mjs
+++ dist/sora.mjs
@@ -1095,7 +1095,21 @@ let LYRA_MODULE;
 /**
  * Lyra の初期化を行うメソッド
  *
- * 詳細は sora.ts の initLyra() メソッドのドキュメントを参照
+ * このメソッドの呼び出し時には設定情報の保存のみを行い、
+ * Lyra での音声エンコード・デコードに必要な WebAssembly ファイルおよびモデルファイルは、
+ * 実際に必要になったタイミングで初めてロードされます
+ *
+ * Lyra を使うためには以下の機能がブラウザで利用可能である必要があります:
+ * - クロスオリジン分離（内部で SharedArrayBuffer クラスを使用しているため）
+ * - WebRTC Encoded Transform
+ *
+ * これらの機能が利用不可の場合には、このメソッドは警告メッセージを出力した上で、
+ * 返り値として false を返します
+ *
+ * @param config Lyra の設定情報
+ * @returns Lyra の初期化に成功したかどうか
+ *
+ * @public
  */
 function initLyra(config) {
     if (!("createEncodedStreams" in RTCRtpSender.prototype)) {
@@ -5165,21 +5179,7 @@ var sora = {
     /**
      * Lyra の初期化を行うメソッド
      *
-     * このメソッドの呼び出し時には設定情報の保存のみを行い、
-     * Lyra での音声エンコード・デコードに必要な WebAssembly ファイルおよびモデルファイルは、
-     * 実際に必要になったタイミングで初めてロードされます
-     *
-     * Lyra を使うためには以下の機能がブラウザで利用可能である必要があります:
-     * - クロスオリジン分離（内部で SharedArrayBuffer クラスを使用しているため）
-     * - WebRTC Encoded Transform
-     *
-     * これらの機能が利用不可の場合には、このメソッドは警告メッセージを出力した上で、
-     * 返り値として false を返します
-     *
-     * @param config Lyra の設定情報
-     * @returns Lyra の初期化に成功したかどうか
-     *
-     * @public
+     * 詳細は lyra.ts の initLyra() メソッドのドキュメントを参照
      */
     initLyra,
     /**
