1b80b292686cdf0e0ed47e42699d26ac1537083c
voluntas <nakai@shiguredo.jp>
2024-02-21 17:02:56 +0900

    片方向だけ

--- tests/messaging.spec.ts
+++ tests/messaging.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '@playwright/test'
 
-test.skip('messaging recvonly pages', async ({ browser }) => {
+test('messaging pages', async ({ browser }) => {
   // 新しいページを2つ作成
   const page1 = await browser.newPage()
   const page2 = await browser.newPage()
@@ -32,17 +32,17 @@ test.skip('messaging recvonly pages', async ({ browser }) => {
   console.log(`Received message on page2: ${receivedMessage1}`)
   test.expect(receivedMessage1).toBe('Hello from page1')
 
-  // page2からpage1へメッセージを送信
-  await page2.fill('input[name="message"]', 'Hello from page2')
-  await page2.click('#send-message')
+  // // page2からpage1へメッセージを送信
+  // await page2.fill('input[name="message"]', 'Hello from page2')
+  // await page2.click('#send-message')
 
-  // page1でメッセージが受信されたことを確認
-  await page1.waitForSelector('li', { state: 'attached' })
-  const receivedMessage2 = await page1.$eval('#received-messages li', (el) => el.textContent)
+  // // page1でメッセージが受信されたことを確認
+  // await page1.waitForSelector('li', { state: 'attached' })
+  // const receivedMessage2 = await page1.$eval('#received-messages li', (el) => el.textContent)
 
-  // 受信したメッセージが期待したものであるか検証
-  console.log(`Received message on page1: ${receivedMessage2}`)
-  test.expect(receivedMessage2).toBe('Hello from page2')
+  // // 受信したメッセージが期待したものであるか検証
+  // console.log(`Received message on page1: ${receivedMessage2}`)
+  // test.expect(receivedMessage2).toBe('Hello from page2')
 
   await page1.click('#stop')
   await page2.click('#stop')
