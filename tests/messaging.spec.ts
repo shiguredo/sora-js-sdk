dc8200e1c3f74a90df1e3619d850ad28ca8c1f20
voluntas <nakai@shiguredo.jp>
2024-03-01 11:33:49 +0900

    送信まで

--- tests/messaging.spec.ts
+++ tests/messaging.spec.ts
@@ -24,25 +24,25 @@ test('messaging pages', async ({ browser }) => {
   await page1.fill('input[name="message"]', 'Hello from page1')
   await page1.click('#send-message')
 
-  // page2でメッセージが受信されたことを確認
-  await page2.waitForSelector('#received-messages li', { state: 'attached' })
-  const receivedMessage1 = await page2.$eval('#received-messages li', (el) => el.textContent)
+  // // page2でメッセージが受信されたことを確認
+  // await page2.waitForSelector('#received-messages li', { state: 'attached' })
+  // const receivedMessage1 = await page2.$eval('#received-messages li', (el) => el.textContent)
 
-  // 受信したメッセージが期待したものであるか検証
-  console.log(`Received message on page2: ${receivedMessage1}`)
-  test.expect(receivedMessage1).toBe('Hello from page1')
+  // // 受信したメッセージが期待したものであるか検証
+  // console.log(`Received message on page2: ${receivedMessage1}`)
+  // test.expect(receivedMessage1).toBe('Hello from page1')
 
-  // page2からpage1へメッセージを送信
-  await page2.fill('input[name="message"]', 'Hello from page2')
-  await page2.click('#send-message')
+  // // page2からpage1へメッセージを送信
+  // await page2.fill('input[name="message"]', 'Hello from page2')
+  // await page2.click('#send-message')
 
-  // page1でメッセージが受信されたことを確認
-  await page1.waitForSelector('li', { state: 'attached' })
-  const receivedMessage2 = await page1.$eval('#received-messages li', (el) => el.textContent)
+  // // page1でメッセージが受信されたことを確認
+  // await page1.waitForSelector('li', { state: 'attached' })
+  // const receivedMessage2 = await page1.$eval('#received-messages li', (el) => el.textContent)
 
-  // 受信したメッセージが期待したものであるか検証
-  console.log(`Received message on page1: ${receivedMessage2}`)
-  test.expect(receivedMessage2).toBe('Hello from page2')
+  // // 受信したメッセージが期待したものであるか検証
+  // console.log(`Received message on page1: ${receivedMessage2}`)
+  // test.expect(receivedMessage2).toBe('Hello from page2')
 
   await page1.click('#stop')
   await page2.click('#stop')
