c78444643d5ab78c5e06b9d2e8974353ae0384e8
blaue-fuchs <hamasakit@gmail.com>
2024-08-28 16:02:51 +0900

    ローカルでのテストは OK

--- tests/sendonly_audio_bit_rate.spec.ts
+++ tests/sendonly_audio_bit_rate.spec.ts
@@ -5,18 +5,23 @@ test('sendonly audioBitRate', async ({ page }) => {
 
   // audioBitRate=指定なしの時 targetBitrate=64000
   await page.click('#start')
+  await page.waitForTimeout(5000)
   await page.click('#get-stats')
+  // 統計情報が表示されるまで待機
   await page.waitForSelector('#stats-report')
+  // データセットから統計情報を取得
   const statsReportJson: Record<string, unknown>[] = await page.evaluate(() => {
-    const statsReportDiv = document.querySelector("#stats-report'") as HTMLDivElement
-    return statsReportDiv ? JSON.parse(statsReportDiv.dataset.statsReportJSON || '[]') : []
+    const statsReportDiv = document.querySelector('#stats-report') as HTMLDivElement
+    return statsReportDiv ? JSON.parse(statsReportDiv.dataset.statsReportJson || '[]') : []
   })
-  const outboundRTP = statsReportJson.filter((report) => report.type === 'outbound-rtp')
-  expect(
-    outboundRTP.find((stats) => {
-      return stats.label === 'targetBitrate' && stats.state === '64000'
-    }),
-  ).toBeDefined()
+  const outboundRtp = statsReportJson.find(
+    (stats) => stats.type === 'outbound-rtp' && stats.kind === 'audio',
+  )
+  expect(outboundRtp).toBeDefined()
+  expect(outboundRtp?.targetBitrate).toEqual(64000)
 
   // audioBitRate=32 の時 targetBitrate=32000
+
+  await page.click('#stop')
+  await page.close()
 })
