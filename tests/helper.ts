dbe985b387dd83e1073e18d7c97d34a07bf16aff
voluntas <nakai@shiguredo.jp>
2024-11-29 17:58:32 +0900

    お試し

--- tests/helper.ts
+++ tests/helper.ts
@@ -1,24 +1,13 @@
-import * as fs from 'node:fs'
-import * as path from 'node:path'
-
-const getPackageVersion = (): string => {
-  const packageJsonPath = path.resolve(__dirname, '../examples/package.json')
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
-  return packageJson.dependencies['sora-js-sdk'].replace('^', '')
-}
-
 // バージョン比較用のヘルパー関数を追加
-export const isVersionGreaterThanOrEqual = (version: string): boolean => {
-  const packageVersion = getPackageVersion()
-
+export const isVersionGreaterThanOrEqual = (packageVersion: string, version: string): boolean => {
   const v1Parts = packageVersion.split('.').map(Number)
   const v2Parts = version.split('.').map(Number)
 
   for (let i = 0; i < Math.max(v1Parts.length, v2Parts.length); i++) {
     const v1 = v1Parts[i] || 0
     const v2 = v2Parts[i] || 0
-    if (v1 < v2) return true
-    if (v1 > v2) return false
+    if (v1 > v2) return true
+    if (v1 < v2) return false
   }
   return true
 }
