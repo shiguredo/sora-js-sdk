0d85584af00b23ab5dfe78852a82e83b0f092814
voluntas <nakai@shiguredo.jp>
2024-12-08 22:51:13 +0900

    統計は不要なので削除

--- tests/type_switched.spec.ts
+++ tests/type_switched.spec.ts
@@ -50,47 +50,6 @@ test('data_channel_signaling_only type:switched pages', async ({ browser }) => {
   const value3 = await msg.args()[2].jsonValue()
   expect(value3).toBe('websocket')
 
-  // 'Get Stats' ボタンをクリックして統計情報を取得
-  await dataChannelSignalingOnly.click('#get-stats')
-
-  // 統計情報が表示されるまで待機
-  await dataChannelSignalingOnly.waitForSelector('#stats-report')
-  // データセットから統計情報を取得
-  const dataChannelSignalingOnlyStatsReportJson: Record<string, unknown>[] =
-    await dataChannelSignalingOnly.evaluate(() => {
-      const statsReportDiv = document.querySelector('#stats-report') as HTMLDivElement
-      return statsReportDiv ? JSON.parse(statsReportDiv.dataset.statsReportJson || '[]') : []
-    })
-
-  // sendonly audio codec
-  const dataChannelSignalingOnlyAudioCodecStats = dataChannelSignalingOnlyStatsReportJson.find(
-    (report) => report.type === 'codec' && report.mimeType === 'audio/opus',
-  )
-  expect(dataChannelSignalingOnlyAudioCodecStats).toBeDefined()
-
-  // sendonly audio outbound-rtp
-  const dataChannelSignalingOnlyAudioOutboundRtp = dataChannelSignalingOnlyStatsReportJson.find(
-    (report) => report.type === 'outbound-rtp' && report.kind === 'audio',
-  )
-  expect(dataChannelSignalingOnlyAudioOutboundRtp).toBeDefined()
-  expect(dataChannelSignalingOnlyAudioOutboundRtp?.bytesSent).toBeGreaterThan(0)
-  expect(dataChannelSignalingOnlyAudioOutboundRtp?.packetsSent).toBeGreaterThan(0)
-
-  // sendonly video codec
-  const dataChannelSignalingOnlyVideoCodecStats = dataChannelSignalingOnlyStatsReportJson.find(
-    (stats) => stats.type === 'codec' && stats.mimeType === 'video/VP9',
-  )
-  expect(dataChannelSignalingOnlyVideoCodecStats).toBeDefined()
-
-  // sendonly video outbound-rtp
-  const dataChannelSignalingOnlyVideoOutboundRtpStats =
-    dataChannelSignalingOnlyStatsReportJson.find(
-      (stats) => stats.type === 'outbound-rtp' && stats.kind === 'video',
-    )
-  expect(dataChannelSignalingOnlyVideoOutboundRtpStats).toBeDefined()
-  expect(dataChannelSignalingOnlyVideoOutboundRtpStats?.bytesSent).toBeGreaterThan(0)
-  expect(dataChannelSignalingOnlyVideoOutboundRtpStats?.packetsSent).toBeGreaterThan(0)
-
   await dataChannelSignalingOnly.click('#disconnect')
 
   await dataChannelSignalingOnly.close()
