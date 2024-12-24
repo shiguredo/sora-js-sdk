ab8bd69bef61265e6b4cdf3ba57f82e59264fdc5
voluntas <nakai@shiguredo.jp>
2024-12-24 17:30:48 +0900

    root が適切に設定されていれば rollupOptions 不要だった

--- examples/vite.config.mjs
+++ examples/vite.config.mjs
@@ -15,25 +15,5 @@ export default defineConfig({
           'sora-js-sdk': resolve(__dirname, '../dist/sora.mjs'),
         },
   },
-  build: {
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, 'index.html'),
-        sendrecv: resolve(__dirname, 'sendrecv/index.html'),
-        sendonly: resolve(__dirname, 'sendonly/index.html'),
-        recvonly: resolve(__dirname, 'recvonly/index.html'),
-        check_stereo: resolve(__dirname, 'check_stereo/index.html'),
-        check_stereo_multi: resolve(__dirname, 'check_stereo_multi/index.html'),
-        replace_track: resolve(__dirname, 'replace_track/index.html'),
-        simulcast: resolve(__dirname, 'simulcast/index.html'),
-        spotlight_sendrecv: resolve(__dirname, 'spotlight_sendrecv/index.html'),
-        spotlight_sendonly: resolve(__dirname, 'spotlight_sendonly/index.html'),
-        spotlight_recvonly: resolve(__dirname, 'spotlight_recvonly/index.html'),
-        sendonly_audio: resolve(__dirname, 'sendonly_audio/index.html'),
-        messaging: resolve(__dirname, 'messaging/index.html'),
-        data_channel_signaling_only: resolve(__dirname, 'data_channel_signaling_only/index.html'),
-      },
-    },
-  },
   envDir: resolve(__dirname, '..'),
 })
