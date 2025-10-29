cdcb319e396d67cf81bbbbe7f0a89f5618e864c8
voluntas <nakai@shiguredo.jp>
2025-10-29 15:32:04 +0900

    ランダムにする

--- e2e-tests/tests/spotlight_sendonly_recvonly.test.ts
+++ e2e-tests/tests/spotlight_sendonly_recvonly.test.ts
@@ -11,20 +11,25 @@ test('spotlight sendonly/recvonly pages', async ({ browser }) => {
   await sendonly.goto('http://localhost:9000/spotlight_sendonly/')
   await recvonly.goto('http://localhost:9000/spotlight_recvonly/')
 
+  const channelName = crypto.randomUUID()
+
+  await sendonly.fill('#channel-name', channelName)
+  await recvonly.fill('#channel-name', channelName)
+
   await sendonly.click('#connect')
   await recvonly.click('#connect')
 
-  // #sendrecv1-connection-id 要素が存在し、その内容が空でないことを確認するまで待つ
+  // #sendonly-connection-id 要素が存在し、その内容が空でないことを確認するまで待つ
   await sendonly.waitForSelector('#connection-id:not(:empty)')
 
   // #sendonly-connection-id 要素の内容を取得
   const sendonlyConnectionId = await sendonly.$eval('#connection-id', (el) => el.textContent)
   console.log(`sendonly connectionId=${sendonlyConnectionId}`)
 
-  // #sendrecv1-connection-id 要素が存在し、その内容が空でないことを確認するまで待つ
+  // #recvonly-connection-id 要素が存在し、その内容が空でないことを確認するまで待つ
   await recvonly.waitForSelector('#connection-id:not(:empty)')
 
-  // #sendrecv1-connection-id 要素の内容を取得
+  // #recvonly-connection-id 要素の内容を取得
   const recvonlyConnectionId = await recvonly.$eval('#connection-id', (el) => el.textContent)
   console.log(`recvonly connectionId=${recvonlyConnectionId}`)
 
